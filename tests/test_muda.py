276bffbab61cc121912e00cee7d0d22443c35182
ellguso <dkiedanski@gmail.com>
2019-06-20 11:52:23 +0200

    fixed muda test

--- tests/test_muda.py
+++ tests/test_muda.py
@@ -28,14 +28,18 @@ def test_example_1(bid_dataset_muda_example_1):
     assert np.allclose(true_fees, fees)
 
     data_trans = [
-           [0  , 1 , 100 , -1 , True]  ,
-           [1  , 1 , 90  , -1 , True]  ,
-           [2  , 1 , 80  , -1 , True]  ,
-           [3  , 1 , 60  , -1 , True]  ,
-           [6  , 1 , 10  , -1 , False] ,
-           [7  , 1 , 20  , -1 , False] ,
-           [11 , 1 , 15  , -1 , False] ,
-           [12 , 1 , 25  , -1 , False] ,
+           [0  , 1 , 50 , -1 , False]  ,
+           [1  , 1 , 50  , -1 , False]  ,
+           [2  , 1 , 50  , -1 , False]  ,
+           [3  , 1 , 50  , -1 , False]  ,
+           [6  , 1 , 50  , -1 , False] ,
+           [7  , 1 , 50  , -1 , False] ,
+           [11 , 1 , 50  , -1 , False] ,
+           [12 , 1 , 50  , -1 , False] ,
             ]
     # Exclude the active colum full of Nones
-    assert np.allclose(data_trans[:, :-1], trans.get_df().values[:, :-1])
+    data_trans = np.array(data_trans).astype(float)
+    df2 = trans.get_df().values.astype(float)
+    print(data_trans)
+    print(df2)
+    assert np.allclose(data_trans, df2)
