ed383469c7623f398c13df7834627fff55694806
ellguso <dkiedanski@gmail.com>
2019-06-25 18:08:00 +0200

    Bump version: 0.5.1 → 0.5.1

--- pymarket/__init__.py
+++ pymarket/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Diego Kiedanki"""
 __email__ = 'gusok@protonmail.com'
-__version__ = '__version__ = '0.5.1''
+__version__ = '__version__ = '__version__ = '0.5.1'''
 
 import pymarket.bids
 import pymarket.mechanisms
--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/gus0k/pymarket',
-    version='version='0.5.1'',
+    version='version='version='0.5.1''',
     zip_safe=False,
 )
