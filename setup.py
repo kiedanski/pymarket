67f777338178ee0ca1ead16039083d77df585141
ellguso <dkiedanski@gmail.com>
2019-06-25 18:03:01 +0200

    Bump version: 0.5.0 → 0.5.1

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/gus0k/pymarket',
-    version='0.5.0',
+    version='0.5.1',
     zip_safe=False,
 )
