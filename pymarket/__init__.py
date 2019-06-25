5ae3c9bd40ce2e72dbf25037f1eeae7603b08a7e
ellguso <dkiedanski@gmail.com>
2019-06-25 20:41:39 +0200

    fixing versions again

--- pymarket/__init__.py
+++ pymarket/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Diego Kiedanki"""
 __email__ = 'gusok@protonmail.com'
-__version__ = '__version__ = '0.5.4''
+__version__ = '0.5.4'
 
 import pymarket.bids
 import pymarket.mechanisms
