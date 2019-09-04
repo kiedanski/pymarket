969f13cd82bf7249abc21101d970fc756e7bf40d
gus0k <gusok@protonmail.com>
2019-09-04 10:08:48 +0200

    still breaking

--- pymarket/conftest.py
+++ pymarket/conftest.py
@@ -3,6 +3,12 @@ import pandas
 import pymarket
 import pytest
 
+pandas.set_option('display.max_rows', len(x))
+pandas.set_option('display.max_columns', None)
+pandas.set_option('display.width', 2000)
+pandas.set_option('display.float_format', '{:20,.2f}'.format)
+pandas.set_option('display.max_colwidth', -1)
+
 @pytest.fixture(autouse=True)
 def add_namespace(doctest_namespace):
         doctest_namespace['np'] = numpy
