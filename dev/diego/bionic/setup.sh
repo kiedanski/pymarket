a9d763f41ac74943b74351d08201b6a5e467eb57
gus0k <gusok@protonmail.com>
2019-10-30 10:31:42 +0800

    second attempt python3.4

---
+++ dev/diego/bionic/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Generic requirements for funtional curl, etc.
+#echo "Provisioning virtual machine..."
+#apt install --yes apt-transport-https
+#apt install --yes ca-certificates
+#apt install --yes software-properties-common
+#apt install --yes curl
+
+# pymarket repository specifics
+echo "Installing pymarket and requirements..."
+
+#apt install --yes python3-pip=9.0.1-2.3~ubuntu1.18.04.1
+apt-get update
+apt-get install --yes  python3-pip=9.0.1-2.3~ubuntu1.18.04.1
+#python3 -m pip install https://github.com/gus0k/pymarket/archive/python3.5.zip
