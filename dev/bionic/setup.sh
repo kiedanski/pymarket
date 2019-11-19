e51f895495aadb42af02fe9e3bcbfe926bae2cf4
Mark Van de Vyver <1335713+taqtiqa-admin@users.noreply.github.com>
2019-11-19 21:20:42 +1100

    Updated to the most recent claimed to work instructions.   Still broken.

--- dev/bionic/setup.sh
+++ dev/bionic/setup.sh
@@ -11,5 +11,5 @@ apt autoremove --yes
 # pymarket repository specifics
 echo "Installing pymarket and requirements..."
 
-apt install --yes python3-pip=9.0.1-2.3~ubuntu1.18.04.1
-apt autoremove --yes
+apt-get update
+apt-get install --yes python3-pip
--- dev/xenial/setup.sh
+++ dev/xenial/setup.sh
@@ -11,4 +11,5 @@ apt install --yes curl
 # pymarket repository specifics
 echo "Installing pymarket and requirements..."
 
-apt install --yes python3-pip=8.1.1-2ubuntu0.4
+apt-get update
+apt-get install --yes python3-pip
